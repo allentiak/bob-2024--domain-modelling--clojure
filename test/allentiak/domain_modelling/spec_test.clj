774b9844ef2cf7c9c10c562c3e2018b143d59e0e
Leandro Doctors <ldoctors@gmail.com>
2024-01-23 00:14:39 +0100

    fixup! refactor: move spec tries to different ns's

--- test/allentiak/domain_modelling/spec_test.clj
+++
@@ -1,4 +0,0 @@
-(ns allentiak.domain-modelling.spec-test
-  (:require [allentiak.domain-modelling.spec :as sut]
-            [clojure.test :refer [deftest testing use-fixtures]]
-            [expectations.clojure.test :refer [expect]]))
