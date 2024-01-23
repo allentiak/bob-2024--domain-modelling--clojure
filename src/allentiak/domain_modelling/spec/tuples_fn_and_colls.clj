754d3895eb6642d6b17640486e5f640724d892f2
Leandro Doctors <ldoctors@gmail.com>
2024-01-23 17:24:49 +0100

    fixup! specs: improve fn instrumentalization

--- src/allentiak/domain_modelling/spec/tuples_fn_and_colls.clj
+++ src/allentiak/domain_modelling/spec/tuples_fn_and_colls.clj
@@ -1,22 +1,26 @@
 (ns allentiak.domain-modelling.spec.tuples-fn-and-colls
-  (:require [clojure.spec.alpha :as s]))
+  (:require
+   [clojure.spec.alpha :as s]
+   [clojure.spec.test.alpha :as st]))
 
 (s/def three-numbers
   (s/coll-of int?
              :count 3))
 
-(s/def :geo/coords
+(s/def :geo/coords1
   (s/tuple double?
            double?))
 
-(s/conform :geo/coords
+(s/conform :geo/coords1
            [20.5 -12.5])
-;; => {:lat 20.5, :long -12.5}
+;; => [20.5 -12.5]
 
-(s/def :geo/coords
+(s/def :geo/coords2
   (s/cat :lat double?
          :long double?))
 
+(s/conform :geo/coords2 [20.5 -12.5])
+;; => {:lat 20.5, :long -12.5}
 
 (s/def :whatever/whatever
   (s/and string?
@@ -25,17 +29,56 @@
 (s/valid? :whatever/whatever "whatever")
 ;; => true
 
-
-(s/def ::length
+(s/def :square/length
   nat-int?)
 
-(s/def ::area
+(s/def :square/area
   nat-int?)
 
 ;; Length --> Area
 
+(defn area-of-square
+  [len]
+  (* len len))
+
+(comment
+  (require '[clojure.repl :as repl :refer [doc]])
+
+  (doc area-of-square)
+  ;; -------------------------
+  ;; allentiak.domain-modelling.spec.tuples-fn-and-colls/area-of-square
+  ;; ([len])
+
+  (area-of-square -1))
+  ;; => 1
+
 (s/fdef area-of-square
-  :args (:len ::length)
-  :ret ::area
+  :args (s/cat :len :square/length)
+  :ret :square/area
   :fn #(>= (:ret %)
            (:len %)))
+
+(comment
+  (require '[clojure.repl :as repl :refer [doc]])
+
+  (require '[clojure.spec.test.alpha :as st])
+
+  (st/instrument `area-of-square)
+
+  ;; enhanced docs
+  (doc area-of-square)
+  ;; -------------------------
+  ;; allentiak.domain-modelling.spec.tuples-fn-and-colls/area-of-square
+  ;; ([len])
+  ;; Spec
+  ;;   args: (cat :len :square/length)
+  ;;   ret: nat-int?
+  ;;   fn: (>= (:ret %) (:len %)))
+  ;; nil
+
+  ;; improved error messages
+  (area-of-square -1))
+  ;; Execution error - invalid arguments to
+  ;; allentiak.domain-modelling.spec.tuples-fn-and-colls/area-of-square
+  ;; at (REPL:1).
+  ;; -1 - failed: nat-int? at: [:len] spec: :square/length
