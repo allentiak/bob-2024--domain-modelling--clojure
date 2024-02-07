7417ec6bcd1295681e0c39f8142f9c29bbd7d14c
Leandro Doctors <ldoctors@gmail.com>
2024-02-07 21:07:18 +0100

    cats: "scientist" is an aggregate, not an entity

--- src/allentiak/domain_modelling/schroedinger_cats/core.clj
+++ src/allentiak/domain_modelling/schroedinger_cats/core.clj
@@ -1,25 +1,25 @@
 (ns allentiak.domain-modelling.schroedinger-cats.core
   (:require [clojure.spec.alpha :as s]))
 
-(s/def :entity/scientist
+(s/def :aggregates/scientist
   string?)
 
 (s/fdef first-scientist-selected
-  :args (s/cat (s/coll-of :entity/scientist))
-  :ret :entity/scientist)
+  :args (s/cat (s/coll-of :aggregates/scientist))
+  :ret :aggregates/scientist)
 
 (s/fdef first-experiment-started
-  :args :entity/scientist
-  :ret :entity/scientist)
+  :args :aggregates/scientist
+  :ret :aggregates/scientist)
 
 (s/fdef deck-shuffled
   :args :entity/unshuffled-deck
   :ret :entity/shuffled-deck)
 
 (s/fdef cards-dealt
-  :args (s/cat (s/coll-of :entity/scientist))
-  :ret :entity/scientist)
+  :args (s/cat (s/coll-of :aggregates/scientist))
+  :ret :aggregates/scientist)
 
 (s/fdef research-deck-created
-  :args (s/cat (s/coll-of :entity/scientist))
-  :ret :entity/scientist)
+  :args (s/cat (s/coll-of :aggregates/scientist))
+  :ret :aggregates/scientist)
